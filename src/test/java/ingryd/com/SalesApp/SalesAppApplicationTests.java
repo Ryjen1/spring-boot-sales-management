26990a0fcf8bc8706b5d920ce9b5922f22662ee9
Maryjane Okafor <ryjenspark@gmail.com>
2024-01-22 19:19:47 +0100

    Initial Commit

---
+++ src/test/java/ingryd/com/SalesApp/SalesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ingryd.com.SalesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
